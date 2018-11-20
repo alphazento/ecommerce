8699235eb400de5e528d1f8189f1bfbc240edc1f
chen <yongcheng.chen@live.com>
2018-11-20 22:20:59 +1100

    update

--- src/Sales/Services/SalesService.php
+++ src/Sales/Services/SalesService.php
@@ -6,12 +6,18 @@ use DB;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 use Zento\Sales\Model\ORM\SalesOrder;
-use Zento\Sales\Model\ORM\SalesAddress;
 use Zento\Sales\Model\ORM\SalesOrderPayment;
+use Zento\Sales\Model\ORM\SalesAddress;
+use Zento\Sales\Model\ORM\SalesShipment;
 
 class SalesService
 {
-  public function placeOrder(\Zento\Contracts\Catalog\Model\ShoppingCart $cart, $payment, $customer_note = '') {
+  public function placeOrder(\Zento\Contracts\Catalog\Model\ShoppingCart $cart, 
+    $payment, 
+    $ship_method_code, 
+    $shipping_carrier = '',
+    $shipping_fee = '',
+    $customer_note = '') {
     $order = SalesOrder::create([
       'invoice_no' => 0,
       'store_id' => 0,
@@ -32,6 +38,46 @@ class SalesService
       'order_currency_code' => 'AUD',
       'base_to_order_currency_rate' => "1.3"
     ]);
+
+    $billing_address = new SalesAddress($cart->billing_address->getAttributes());
+    $billing_address->id = null;
+    $billing_address->save();
+
+    if ($cart->ship_to_billingaddesss) {
+      $shipping_address = $billing_address;
+    } else {
+      $shipping_address = new SalesAddress($cart->shipping_address->getAttributes());
+      $shipping_address->id = null;
+      $shipping_address->save();
+    }
+
+    $shipment = SalesShipment::create([
+      'order_id' => $order->id,
+      'customer_id' => $cart->customer_id,
+      'shipment_status' => 0,
+      'shipping_address_id' => $shipping_address->id,
+      'billing_address_id' => $billing_address->id,
+      'shipping_method' => 0,
+      'shipping_carrier' => 0,
+      'shipment_instruction' => 0,
+      'total_weight' => 0,
+      'total_qty' => $cart->items_quantity,
+    ]);
+
+    $shipment->save();
+
+    $payment = SalesOrderPayment::create([
+      'order_id' => $order->id,
+      'comment' => '',
+      'total_due' => 0,
+      'amount_authorized' => $cart->total,
+      'amount_paid' => $cart->total,
+      'amount_refunded' => 0,
+      'amount_canceled' => 0,
+    ]);
+    $payment->save();
+    $payment->storePaymentItems($cart);
+
     return $order;
   }
 }
