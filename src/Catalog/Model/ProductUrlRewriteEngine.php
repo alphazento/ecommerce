3bfbabc0a9627c664c78560b4f892cf6089f25eb
Yongcheng Chen <tony@tonercity.com.au>
2018-11-30 17:30:23 +1100

    update

--- src/Catalog/Model/ProductUrlRewriteEngine.php
+++ src/Catalog/Model/ProductUrlRewriteEngine.php
@@ -7,10 +7,16 @@ use Zento\RouteAndRewriter\Model\UrlRewriteRule as RuleModel;
 use Zento\RouteAndRewriter\Facades\RouteAndRewriterService;
 use Zento\Catalog\Model\ORM\Product;
 
-class ProductUrlRewriteEngine extends \Zento\RouteAndRewriter\Engine\UrlRewriteEngine
+class ProductUrlRewriteEngine extends \Zento\RouteAndRewriter\Engine\UrlRewriteEngineAbstract
 {
     public function findRewriteRule(string $url) {
-        $product = Product::where('url_path', '=', $url)
-            ->first(['id']);
+        if ($product = Product::where('url_path', '=', $url)
+            ->first(['id'])) {
+            $rule = new RuleModel();
+            $rule->to_uri = '/product/' . $product->id;
+            $rule->status_code = 200;
+            return $rule;
+        }
+        return false;
     }
 }
