1727a8ee912e153ab4dd3845714136260a1642fe
chen <chen@192-168-1-8.tpgi.com.au>
2019-02-11 22:43:42 +1100

    update

--- src/Passport/Http/Middleware/CORS.php
+++ src/Passport/Http/Middleware/CORS.php
@@ -14,13 +14,12 @@ class CORS
     public function handle($request, \Closure $next)
     {
         return tap($next($request), function ($response) use ($request) {
-            // $origin = $request->header('origin') ?: $request->url();
             $origin = $request->header('origin') ?: $request->server('HTTP_ORIGIN');
             $response->header('Access-Control-Allow-Origin', $origin);
-            //guest-guid is a customize heaer
-            $response->header('Access-Control-Allow-Headers', 'origin, content-type, accept, authorization, guest-guid, Access-Control-Allow-Origin');
-            $response->header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PATCH, DELETE');
+            //guest-uuid is a customize heaer
+            $response->header('Access-Control-Allow-Headers', 'origin, content-type, accept, authorization, guest-uuid, Access-Control-Allow-Origin');
+            $response->header('Access-Control-Allow-Methods', 'GET, PUT, HEAD, OPTIONS, POST, PATCH, DELETE');
             $response->header('Access-Control-Allow-Credentials', 'true');
-            });
+        });
     }
 }
