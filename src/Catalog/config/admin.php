48e6f6b09e4dc2974ed749797ddc3e1f7673c5bf
chen <chen@192-168-1-5.tpgi.com.au>
2019-03-04 22:18:37 +1100

    update

--- src/Catalog/config/admin.php
+++ src/Catalog/config/admin.php
@@ -3,6 +3,7 @@
 namespace Zento\Catalog\Config;
 
 use Zento\Backend\Providers\Facades\AdminService;
+use Zento\Kernel\Booster\Database\Eloquent\DA\ORM\DynamicAttribute;
 
 class Admin extends \Zento\Backend\Config\AbstractAdminConfig {
     public function registerMenus() {
@@ -10,46 +11,61 @@ class Admin extends \Zento\Backend\Config\AbstractAdminConfig {
 
     protected function _registerGroups($groupTag, &$groups) {
         $groups['catalog/category'] = function($groupTag) {
+            $items[] = [
+                'title' => 'Category Name',
+                'type' => 'Text',
+                'cpath' => 'name'
+            ];
+
+            $itemsGroups = [];
+
+            $dynAttrs = DynamicAttribute::where('parent_table', 'categories')
+                ->where('enabled', 1)
+                ->get();
+
+            foreach($dynAttrs as $item) {
+                if (empty($item->admin_group)) {
+                    $items[] = [
+                        'title' => empty($item->admin_label) ? $item->attribute_name : $item->admin_label,
+                        'type' => empty($item->admin_component) ? 'Text' : $item->admin_component,
+                        'cpath' => $item->attribute_name
+                    ];
+                } else {
+                    $group = $item->admin_group;
+                    if (!isset($itemsGroups[$group])) {
+                        $itemsGroups[$group] = [];
+                    }
+                    $itemsGroups[$group][] = [
+                        'title' => empty($item->admin_label) ? $item->attribute_name : $item->admin_label,
+                        'type' => empty($item->admin_component) ? 'Text' : $item->admin_component,
+                        'cpath' => $item->attribute_name
+                    ];
+                }
+            }
+            
             AdminService::registerGroup($groupTag, 'category',  [
                 'title' => 'Category Settings',
-                'items' => [
-                    [
-                        'title' => 'Enable Category',
-                        'type' => 'Switch',
-                        'cpath' => 'is_active'
-                    ],
-                    [
-                        'title' => 'Include in Menu',
-                        'type' => 'Switch',
-                        'cpath' => 'include_in_menu'
-                    ],
-                    [
-                        'title' => 'Category Name',
-                        'type' => 'Text',
-                        'cpath' => 'name'
-                    ]
-                ]
+                'items' => $items
             ]);
-            AdminService::registerSubgroupToGroup($groupTag, 'category', 'content', [
-                'title' => 'Content',
-                'items' => [
+            foreach($itemsGroups as $group => $items) {
+                AdminService::registerSubgroupToGroup($groupTag, 
+                    'category', 
+                    md5($group), 
                     [
-                        'title' => 'Category Image',
-                        'type' => 'LongText',
-                        'cpath' => 'meta_description'
-                    ]
-                ]
-            ]);
-            AdminService::registerSubgroupToGroup($groupTag, 'category', 'seo', [
-                'title' => 'Search Engine Optimization ',
-                'items' => [
-                    [
-                        'title' => 'Url Key',
-                        'type' => 'LongText',
-                        'cpath' => 'url_key'
-                    ]
-                ]
-            ]);
+                        'title' => $group,
+                        'items' => $items
+                    ]);
+            }
+            // AdminService::registerSubgroupToGroup($groupTag, 'category', 'seo', [
+            //     'title' => 'Search Engine Optimization ',
+            //     'items' => [
+            //         [
+            //             'title' => 'Url Key',
+            //             'type' => 'LongText',
+            //             'cpath' => 'url_key'
+            //         ]
+            //     ]
+            // ]);
         };
     }
 }
