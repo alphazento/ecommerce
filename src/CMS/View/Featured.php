12b3cd3233377ee11e4005ca82996f915b209928
chen <chen@192-168-1-6.tpgi.com.au>
2019-03-23 08:05:56 +1100

    update

--- src/CMS/View/Featured.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zento\CMS\View;
-
-class Featured {
-	public function load($settings) {
-		return ['extension.module.featured' , ['products' => \Zento\Catalog\Model\ORM\Product::limit(4)->get()]];
-	}
-}
--- src/MainTheme/resources/views/page/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends(config('home.layout', 'layout.default'))
-
-@section('content')
-  @foreach($content_top ?? [] as $module)
-    @include($module[0], $module[1])
-  @endforeach
-@endsection
