d4ab96d8548521ed8528789806efa94108b8b244
chen <chen@chens-MacBook-Pro.local>
2019-10-30 19:59:04 +1100

    update

--- src/VueDesktopTheme/resources/vue/._app.dev.js
+++
@@ -1,9 +0,0 @@
-var Vue = window.Vue;
-import Zento_VueDesktopTheme_Configs from "@Zento_VueDesktopTheme/_components.js"
-
-for (const [key, value] of Object.entries(Zento_VueDesktopTheme_Configs)) {
-    Vue.component(
-        key,
-        () => import(`@Zento_VueDesktopTheme/${value}` /* webpackChunkName:"vue-dev-watch/zento-vuedesktoptheme" */ )
-    );
-}
